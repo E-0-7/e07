e5c74448dddff5cde7a44495cd6ca5a8bed20969
Thirza <thirzahmad@gmail.com>
2023-10-26 20:05:20 +0700

    menambahkan data di fixture

--- katalog_buku/models.py
+++ katalog_buku/models.py
@@ -2,8 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Buku(models.Model):
-    judul = models.CharField(max_length=255)
-    image = models.URLField()
-    penulis = models.CharField(max_length=255)
-    tahun_terbit = models.IntegerField()
-    jumlah = models.IntegerField()
+    isbn = models.CharField(max_length=255, null=True, blank=True)
+    book_title = models.CharField(max_length=255, null=True, blank=True)
+    book_author = models.CharField(max_length=255, null=True, blank=True)
+    tahun_publikasi = models.IntegerField(null=True, blank=True)
+    penerbit = models.CharField(max_length=255, null=True, blank=True)
+    url_foto_kecil = models.URLField(null=True, blank=True)
+    url_foto_medium = models.URLField(null=True, blank=True)
+    url_foto_large = models.URLField(null=True, blank=True)
