0e5895baef35fe95454c0ec7bdda678cce1dcc78
Caesar Justitio <caesarjustitio17@gmail.com>
2023-10-26 16:27:27 +0700

    Menambah model.py

--- book/models.py
+++ book/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Books(models.Model):
+    ISBN = models.IntegerField();
+    Book_Title = models.CharField(max_length=255);
+    Book_Author = models.CharField(max_length=255);
+    Year_Publication = models.IntegerField();
+    Publisher = models.CharField(max_length=255);
+    Book_Cover = "http://images.amazon.com/images/P/0195153448.01.MZZZZZZZ.jpg",
