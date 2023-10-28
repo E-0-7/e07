e55e7db8be6468f01b5cd00a86b4739e9e1b63fb
Thirza <thirzahmad@gmail.com>
2023-10-28 23:27:53 +0700

    menambahkan fungsionalitas search dan fix url ke modul

--- katalog_buku/urls.py
+++ katalog_buku/urls.py
@@ -1,10 +1,11 @@
-from .views import get_books, get, add_product_ajax
+from .views import get_books, get, add_product_ajax, search
 from django.urls import path
 
 app_name = "katalog_buku"
 
 urlpatterns = [
     path("json/", get_books, name="get_books"),
-    path('', get, name="name"),
-    path('/tambah-buku/', add_product_ajax, name="add_product_ajax")
+    path('', get, name="get"),
+    path('search/', search, name="search"),
+    path('tambah-buku/', add_product_ajax, name="add_product_ajax")
 ]
--- register/urls.py
+++ register/urls.py
@@ -8,6 +8,5 @@ app_name="register"
 urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
-    path('request_buku/', status_request_buku, name='status_request_buku'),
     path('register/', register, name='register'),
 ]
