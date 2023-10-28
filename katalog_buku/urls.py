4bff1cfa80dfd7a677ecb024d97cb20f33ea3370
Merrick2q <rickysetiawan926H@gmail.com>
2023-10-28 11:46:26 +0700

    Katalog buku

--- katalog_buku/urls.py
+++ katalog_buku/urls.py
@@ -1,4 +1,4 @@
-from .views import get_books, get
+from .views import get_books, get, add_product_ajax
 from django.urls import path
 
 app_name = "katalog_buku"
@@ -6,4 +6,5 @@ app_name = "katalog_buku"
 urlpatterns = [
     path("json/", get_books, name="get_books"),
     path('', get, name="name"),
+    path('tambah-buku/', add_product_ajax, name="add_product_ajax")
 ]
