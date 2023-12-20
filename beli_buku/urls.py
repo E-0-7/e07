edeece31d1c6e2707aa9e1f12cfde1f75d584c30
Caesar Justitio <caesarjustitio17@gmail.com>
2023-12-20 22:11:39 +0700

    perbaikan

--- beli_buku/urls.py
+++ beli_buku/urls.py
@@ -3,10 +3,10 @@ from .views import list_beli_buku, get_beli_data_ajax, add_beli_buku_ajax, get_k
 app_name = 'beli_buku'
 
 urlpatterns = [
-    path('list_beli_buku', list_beli_buku, name='list_beli_buku'),
+    path('list_beli_buku/', list_beli_buku, name='list_beli_buku'),
     path('get_beli_data_ajax/', get_beli_data_ajax, name='get_beli_data_ajax'),
     path('add_beli_buku_ajax/<int:id>', add_beli_buku_ajax, name='add_beli_buku_ajax'),
-    path('get_katalog_beli_buku', get_katalog_beli_buku, name='get_katalog_beli_buku'),
+    path('get_katalog_beli_buku/', get_katalog_beli_buku, name='get_katalog_beli_buku'),
     path('', display_beli_buku, name='display_beli_buku'),
     path('create_beli_buku/', create_beli_buku, name='create_beli_buku'),
     path('get_beli_buku/', get_beli_buku, name='get_beli_buku'),
